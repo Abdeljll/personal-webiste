37f118aea5482bd48a41801042ac60c6578191f1
abdelaziz.jalal <ga091908@ens.uqam.ca>
2025-02-03 22:44:08 -0500

    Modification de logo.js pour mettre l'image de mon logo Relay à la place du J de Jalal

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from './relaylogo.png';
 
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
@@ -13,9 +14,13 @@ const IconLogo = () => (
           strokeLinejoin="round"
           points="39 0 0 22 0 67 39 90 78 68 78 23"
         />
-        <path
-          d="M50 30 L50 60 Q50 70 40 70 Q30 70 30 60 L30 55 L35 55 L35 60 Q35 65 40 65 Q45 65 45 60 L45 30 Z"
-          fill="currentColor"
+        <image
+          href={image}
+          x="-1"
+          y="5"
+          width="80"
+          height="80"
+          preserveAspectRatio="xMidYMid meet"
         />
       </g>
     </g>
